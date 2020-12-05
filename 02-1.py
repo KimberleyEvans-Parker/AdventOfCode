5be3512a174ad57227a83fc68ac5b00c59d5d4d0
Kimberley <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2020-12-05 16:56:52 +1300

    Complete Day 02 Part 1

---
+++ 02-1.py
@@ -0,0 +1,13 @@
+with open("02-1-input.txt", "r") as f:
+    policies = f.readlines()
+
+count = 0
+
+for policy in policies:
+    letter = policy.split()[1][0]
+    min_num, max_num = map(int, policy.split()[0].split("-"))
+    code = policy.split()[-1]
+    if code.count(letter) >= min_num and code.count(letter) <= max_num:
+        count += 1
+
+print(count)
