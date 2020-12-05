f41e288a29dd1d923d85c374d22277d939b16a7d
Kimberley <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2020-12-05 15:32:06 +1300

    Complete Day 1 Part 2

---
+++ 01-2.py
@@ -0,0 +1,11 @@
+with open("01-1-input.txt", "r") as f:
+    numbers = list(map(int, f.readlines()))
+
+for number in numbers:
+    for number2 in numbers:
+        for number3 in numbers:
+            if number + number2 + number3 == 2020:
+                ans = number * number2 * number3
+                break
+
+print(ans)
