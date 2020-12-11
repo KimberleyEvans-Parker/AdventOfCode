71b85ae4d780423f18bd11d21cd5f5deaf641f9b
Kimberley <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2020-12-11 23:55:21 +1300

    Remove prints

--- 10-2.py
+++ 10-2.py
@@ -3,22 +3,18 @@ with open("10-input.txt", "r") as f:
 
 nums.append(0)
 nums.sort()
-# print(nums[-1])
 
 current_paths = {0:1}
 count = 0
 
 while len(current_paths) > 0:
-    # print(current_paths)
     curr = min(current_paths.keys())
     amount = current_paths.pop(curr)
-    # print(curr, nums[-1])
     if curr == nums[-1]:
         count += amount
     else:
         for num in nums:
             if num > curr and num <= curr + 3:
-                # print(curr, num)
                 if num in current_paths.keys():
                     current_paths[num] += amount
                 else:
